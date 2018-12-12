942120a5101b98c2f84cd67af1d061f682f6dc25
Noah Gentile <noah.gentile@gmail.com>
2018-12-12 11:41:28 -0500

    Refactoring Histogram to use computed properties

--- src/components/Histogram.vue
+++ src/components/Histogram.vue
@@ -1,143 +1,204 @@
 <template>
-  <svg class='histogram' :width='width' :height='height'>
-    <g class='bars'>
-      <rect v-for='d in bars' :key='d.id' :x='d.x' :width='d.width'
-        :y='d.y' :height='d.height' :fill=d.fill :stroke='d.fill' />
+  <svg class="histogram" :width="width" :height="height">
+    <g class="bars">
+      <rect
+        v-for="({
+          x, y, width, height, fill,
+        }, i) in bars"
+        :key="i"
+        v-bind="{
+          x,
+          y,
+          width,
+          height,
+          fill,
+          stroke
+        }"
+      ></rect>
     </g>
-    <g ref='xAxis' :transform='`translate(0, ${height - margin.bottom})`' />
-    <g ref='brush' />
+    <g class="axis" :transform="`translate(0, ${height - margin.bottom})`">
+      <g class="domain">
+        <path :d="axis" fill="none" stroke="black"></path>
+        <line
+          v-for="(tick, i) in ticks"
+          :key="i"
+          class="tick"
+          :x1="scaleX(tick)"
+          :x2="scaleX(tick)"
+          :y2="10"
+          stroke="black"
+        ></line>
+      </g>
+      <g class="ticks" transform="translate(0, 20)">
+        <template v-for="tick in ticks">
+          <text
+            :key="tick"
+            class="label"
+            :x="scaleX(tick)"
+            y="0"
+            text-anchor="middle"
+          >{{ format(tick) }}</text>
+        </template>
+      </g>
+    </g>
+    <g ref="brush"></g>
   </svg>
 </template>
 
 <script>
-import {
-  axisBottom,
-  brushX,
-  extent,
-  histogram,
-  interpolateViridis,
-  max,
-  median,
-  scaleLinear,
-  scaleSequential,
-  select,
-} from 'd3';
-import {TweenMax} from 'gsap';
+  import {
+    brushX,
+    extent,
+    histogram,
+    interpolateViridis,
+    max,
+    median,
+    scaleLinear,
+    scaleSequential,
+    select
+  } from "d3";
+  import { TweenMax } from "gsap";
 
-const width = 500
-const height = 120
-const margin = {top: 20, right: 20, bottom: 20, left: 20}
+  const width = 500;
+  const height = 120;
+  const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 
-export default {
-  name: 'histogram',
-  props: ['movies', 'filtered', 'id', 'format', 'updateFilters'],
-  data() {
-    return {
-      width, height, margin,
-      bars: [],
-    }
-  },
-  created() {
-    this.xScale = scaleLinear().range([margin.left, width - margin.right]);
-    this.histogram = histogram();
-
-    this.yScale = scaleLinear().range([height - margin.bottom, margin.top]);
-    this.colorScale = scaleSequential(interpolateViridis);
-
-    this.xAxis = axisBottom().scale(this.xScale).tickFormat(this.format);
-    this.brush = brushX().extent([
-      [margin.left, margin.top],
-      [width - margin.right, height - margin.bottom]
-    ]).on('brush', this.brushEnd)
-    .on('end', this.brushEnd)
-  },
-  mounted() {
-    select(this.$refs.brush).call(this.brush);
-  },
-  watch: {
-    movies: function() {
-      this.calculateScales();
-      select(this.$refs.xAxis).call(this.xAxis);
-    },
-    filtered: function() {
-      this.calculateData();
-    }
-  },
-  methods: {
-    calculateScales: function() {
-      if (!this.movies.length) return;
-
-      // colors
-      const colorDomain = extent(this.movies, d => d.score);
-      this.colorScale.domain(colorDomain).nice();
-
-      // x is the attribute
-      const xDomain = extent(this.movies, d => d[this.id]);
-      this.xScale.domain(xDomain).nice();
-
-      // get the bins
-      this.histogram.domain(this.xScale.domain())
-        .thresholds(this.xScale.ticks(50))
-        .value(d => d[this.id]);
-      const bins = this.histogram(this.movies);
-
-      // calculate y from the bins
-      const yMax = max(bins, d => d.length);
-      this.yScale.domain([0, yMax]);
-    },
-    calculateData: function() {
-      if (!this.filtered.length) return;
-
-      // use filtered for bins now that scales are calculated
-      const bins = this.histogram(this.filtered);
-
-      // calculate rect bar for each bin
-      this.bars = bins.map((d, i) => {
-        const {x0, x1} = d;
-        const x = this.xScale(x0);
-        const y = this.bars[i] ? this.bars[i].toY : this.yScale(0);
-        const toY = this.yScale(d.length);
-        const fill = this.colorScale(median(d, d => d.score) || 0);
-
-        return {
-          id: i,
-          x,
-          width: this.xScale(x1) - x,
-          y, toY,
-          height: height - margin.bottom - y,
-          toHeight: height - margin.bottom - toY,
-          fill,
-        };
-      });
-
-      // and then animate them
-      TweenMax.staggerFromTo(this.bars, 0.25, {
-        // from
-        cycle: {
-          y: (i) => this.bars[i].y,
-          height: (i) => this.bars[i].height,
-        }
-      }, {
-        // to
-        cycle: {
-          y: (i) => this.bars[i].toY,
-          height: (i) => this.bars[i].toHeight,
-        },
-      })
-    },
-    brushEnd: function() {
-      let bounds = null;
-      if (event.selection) {
-        const [x1, x2] = event.selection;
-        bounds = [
-          this.xScale.invert(x1),
-          this.xScale.invert(x2),
-        ]
+  export default {
+    name: "histogram",
+    props: {
+      movies: {
+        type: Array,
+        default: () => []
+      },
+      filtered: {
+        type: Array,
+        default: () => []
+      },
+      id: {
+        type: String
+      },
+      format: {
+        type: Function,
+        default: a => a
+      },
+      updateFilters: {
+        type: Function,
+        default: () => {}
       }
-      this.updateFilters({[this.id]: bounds});
     },
-  }
-}
+    data() {
+      return {
+        width,
+        height,
+        margin
+      };
+    },
+    computed: {
+      domainX() {
+        return extent(this.movies, d => d[this.id]);
+      },
+      scaleX() {
+        return scaleLinear()
+          .range([this.margin.left, this.width - this.margin.right])
+          .domain(this.domainX)
+          .nice();
+      },
+      ticks() {
+        return this.scaleX.ticks();
+      },
+      axis() {
+        const range = this.scaleX.range();
+        return `M${range[0] + 0.5},6V0.5H${range[range.length - 1] + 0.5}V6`;
+      },
+      maxY() {
+        const bins = this.histogram(this.movies);
+        return max(bins, d => d.length);
+      },
+      scaleY() {
+        return scaleLinear()
+          .range([this.height - this.margin.bottom, this.margin.top])
+          .domain([0, this.maxY]);
+      },
+      domainZ() {
+        return extent(this.movies, d => d.score);
+      },
+      scaleZ() {
+        return scaleSequential(interpolateViridis)
+          .domain(this.domainZ)
+          .nice();
+      },
+      histogram() {
+        return histogram()
+          .domain(this.domainX)
+          .thresholds(this.scaleX.ticks(50))
+          .value(d => d[this.id]);
+      },
+      bars() {
+        if (!this.filtered.length) return;
+
+        // calculate rect bar for each bin
+        return this.histogram(this.filtered).map((d, i) => {
+          const { x0, x1, length } = d;
+          const x = this.scaleX(x0);
+          const y = this.scaleY(length);
+          const fill = this.scaleZ(median(d, d => d.score) || 0);
+
+          return {
+            id: i,
+            x,
+            y,
+            width: this.scaleX(x1) - x,
+            height: height - margin.bottom - y,
+            fill
+          };
+        });
+      }
+    },
+    watch: {
+      bars: {
+        handler(newBars, oldBars) {
+          // eslint-disable-next-line
+          console.log(newBars);
+          return new TweenMax(
+            this.bars,
+            25,
+            {
+              cycle: {
+                height: i => (oldBars ? oldBars[i].height : 0)
+              }
+            },
+            {
+              cycle: {
+                height: i => newBars[i].height
+              }
+            }
+          );
+        }
+      }
+    },
+    created() {
+      this.brush = brushX()
+        .extent([
+          [margin.left, margin.top],
+          [width - margin.right, height - margin.bottom]
+        ])
+        .on("brush", this.brushEnd)
+        .on("end", this.brushEnd);
+    },
+    mounted() {
+      select(this.$refs.brush).call(this.brush);
+    },
+    methods: {
+      brushEnd: function() {
+        let bounds = null;
+        if (event.selection) {
+          const [x1, x2] = event.selection;
+          bounds = [this.scaleX.invert(x1), this.scaleX.invert(x2)];
+        }
+        this.updateFilters({ [this.id]: bounds });
+      }
+    }
+  };
 </script>
 
 <style scoped>
