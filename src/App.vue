5ab14bf83850bdccddd6db08d872703f5a482099
Noah Gentile <noah.gentile@gmail.com>
2018-12-12 11:41:53 -0500

    Applying Prettier to source code

--- src/App.vue
+++ src/App.vue
@@ -2,104 +2,110 @@
   <div id="app">
     <!-- HEADER -->
     <h1>The Seasonality of Box Office Hits</h1>
-    <p class='description'>
-In the last decade, most block buster hits have happened around or during the summer and winter holidays.
-<strong>Brush</strong> the histograms to filter the movies by metascore and/or box office figures, and <strong>hover</strong> the movies to see more details.
+    <p class="description">
+      In the last decade, most block buster hits have happened around or during the summer and winter holidays.
+      <strong>Brush</strong> the histograms to filter the movies by metascore and/or box office figures, and
+      <strong>hover</strong> the movies to see more details.
     </p>
     <!-- AREA CHART -->
-    <div class='area-container'>
-      <div class='area-title'>
+    <div class="area-container">
+      <div class="area-title">
         <strong>$ over/under median box office</strong>
       </div>
-      <AreaChart v-bind='{movies, filtered}' />
+      <AreaChart v-bind="{movies, filtered}"/>
     </div>
     <!-- HISTOGRAM -->
-    <div class='histograms' v-for='({id, label, format}) in histograms' :key="id">
-      <Histogram v-bind='{movies, filtered, id, format, updateFilters}' />
+    <div class="histograms" v-for="({id, label, format}) in histograms" :key="id">
+      <Histogram v-bind="{movies, filtered, id, format, updateFilters}"/>
       <div>
         <strong>{{ label }}</strong>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import {
-  chain,
-  every,
-} from 'lodash';
-import AreaChart from './components/AreaChart';
-import Histogram from './components/Histogram';
+  import { chain, every, filter } from "lodash";
+  import AreaChart from "./components/AreaChart";
+  import Histogram from "./components/Histogram";
 
-const startYear = 2008;
+  const startYear = 2008;
 
-export default {
-  name: 'app',
-  components: {
-    Histogram, AreaChart,
-  },
-  data() {
-    return {
-      histograms: [
-        {id: 'score', label: 'metascores'},
-        {id: 'boxOffice', label: 'box office figures', format: d => `$${parseInt(d/ 1000000)}M`},
-      ],
-      movies: [],
-      filters: {},
-      filtered: [],
+  export default {
+    name: "app",
+    components: {
+      Histogram,
+      AreaChart
+    },
+    data() {
+      return {
+        histograms: [
+          { id: "score", label: "metascores" },
+          {
+            id: "boxOffice",
+            label: "box office figures",
+            format: d => `$${parseInt(d / 1000000)}M`
+          }
+        ],
+        movies: [],
+        filters: {},
+        filtered: []
+      };
+    },
+    mounted() {
+      fetch(`./movies.json`)
+        .then(resp => resp.json())
+        .then(movies => {
+          this.movies = chain(movies)
+            .map((d, id) => Object.assign(d, { id, date: new Date(d.date) }))
+            .filter(d => d.boxOffice && d.year >= startYear)
+            .value();
+          this.filtered = this.movies;
+        });
+    },
+    computed: {
+      // filtered: function() {
+      //   return filter(this.movies, d => every(this.filters, (bounds, key) =>
+      //     !bounds || bounds[0] < d[key] && d[key] < bounds[1]))
+      // }
+    },
+    methods: {
+      updateFilters: function(params) {
+        this.filters = Object.assign(this.filters, params);
+        this.filtered = filter(this.movies, d =>
+          every(
+            this.filters,
+            (bounds, key) => !bounds || (bounds[0] < d[key] && d[key] < bounds[1])
+          )
+        );
+      }
     }
-  },
-  mounted() {
-    fetch(`./movies.json`)
-      .then(resp => resp.json())
-      .then(movies => {
-        this.movies = chain(movies)
-          .map((d, id) => Object.assign(d, {id, date: new Date(d.date)}))
-          .filter(d => d.boxOffice && d.year >= startYear)
-          .value();
-        this.filtered = this.movies;
-      });
-  },
-  // computed: {
-  //   filtered: function() {
-  //     return filter(this.movies, d => every(this.filters, (bounds, key) =>
-  //       !bounds || bounds[0] < d[key] && d[key] < bounds[1]))
-  //   }
-  // },
-  methods: {
-    updateFilters: function(filter) {
-      this.filters = Object.assign(this.filters, filter);
-      this.filtered = filter(this.movies, d =>
-        every(this.filters, (bounds, key) => !bounds || bounds[0] < d[key] && d[key] < bounds[1]));
-    }
-  }
-}
+  };
 </script>
 
 <style>
-#app {
-  width: 1000px;
-  margin: auto;
-  text-align: center;
-}
+  #app {
+    width: 1000px;
+    margin: auto;
+    text-align: center;
+  }
 
-.description {
-  width: 600px;
-  line-height: 2;
-  margin: 20px auto;
-}
+  .description {
+    width: 600px;
+    line-height: 2;
+    margin: 20px auto;
+  }
 
-.area-container {
-  position: relative;
-}
+  .area-container {
+    position: relative;
+  }
 
-.area-title {
-  position: absolute;
-  left: 65px;
-}
+  .area-title {
+    position: absolute;
+    left: 65px;
+  }
 
-.histograms {
-  display: inline-block;
-}
+  .histograms {
+    display: inline-block;
+  }
 </style>
--- src/components/AreaChart.vue
+++ src/components/AreaChart.vue
@@ -1,174 +1,207 @@
 <template>
-  <div class='areachart'>
-    <svg :width='width' :height='height'>
-      <transition-group tag='g' :css='false' @enter='enter' @leave='leave'>
-        <path v-for='d in arcs' class='arc' :key='d.id' :d='d.path' :fill='d.fill' stroke='#fff'
-          @mouseenter='() => hovered = d' @mouseleave='() => hovered = null' />
+  <div class="areachart">
+    <svg :width="width" :height="height">
+      <transition-group tag="g" :css="false" @enter="enter" @leave="leave">
+        <path
+          v-for="d in arcs"
+          class="arc"
+          :key="d.id"
+          :d="d.path"
+          :fill="d.fill"
+          stroke="#fff"
+          @mouseenter="() => hovered = d"
+          @mouseleave="() => hovered = null"
+        ></path>
       </transition-group>
-      <g ref='xAxis' :transform='`translate(0, ${y0})`' />
-      <g ref='yAxis' :transform='`translate(${margin.left}, 0)`' />
+      <g ref="xAxis" :transform="`translate(0, ${y0})`"></g>
+      <g ref="yAxis" :transform="`translate(${margin.left}, 0)`"></g>
     </svg>
-    <div v-if='hovered' class='hovered' :style='{
+    <div
+      v-if="hovered"
+      class="hovered"
+      :style="{
       top: `${hovered.y + 20}px`,
       left: `${hovered.x}px`,
-      }'>
-      <strong>title</strong> {{ hovered.data.title }}<br />
-      <strong>date</strong> {{ formatDate(hovered.data.date) }}<br />
-      <strong>metascore</strong> {{ hovered.data.score }}<br />
-      <strong>box office</strong> {{ formatBox(hovered.data.boxOffice) }}<br />
+      }"
+    >
+      <strong>title</strong>
+      {{ hovered.data.title }}
+      <br>
+      <strong>date</strong>
+      {{ formatDate(hovered.data.date) }}
+      <br>
+      <strong>metascore</strong>
+      {{ hovered.data.score }}
+      <br>
+      <strong>box office</strong>
+      {{ formatBox(hovered.data.boxOffice) }}
+      <br>
     </div>
   </div>
 </template>
 
 <script>
-import {
-  chain,
-} from 'lodash';
-import {
-  area,
-  axisBottom,
-  axisLeft,
-  curveCatmullRom,
-  extent,
-  format,
-  interpolateViridis,
-  median,
-  scaleLinear,
-  scaleSequential,
-  scaleTime,
-  select,
-  timeFormat,
-  timeMonth,
-} from 'd3';
-import {TweenLite} from 'gsap';
+  import { chain } from "lodash";
 
-const width = 1000;
-const height = 300;
-const margin = {top: 0, right: 0, bottom: 20, left: 60};
+  import {
+    area,
+    axisBottom,
+    axisLeft,
+    curveCatmullRom,
+    extent,
+    format,
+    interpolateViridis,
+    median,
+    scaleLinear,
+    scaleSequential,
+    scaleTime,
+    select,
+    timeFormat,
+    timeMonth
+  } from "d3";
+  import { TweenLite } from "gsap";
 
-export default {
-  name: 'areachart',
-  props: ['movies', 'filtered'],
-  data() {
-    return {
-      width, height, margin,
-      arcs: [],
-      medianBox: 0,
-      y0: 0,
-      hovered: null,
+  const width = 1000;
+  const height = 300;
+  const margin = { top: 0, right: 0, bottom: 20, left: 60 };
+
+  export default {
+    name: "areachart",
+    props: ["movies", "filtered"],
+    data() {
+      return {
+        width,
+        height,
+        margin,
+        arcs: [],
+        medianBox: 0,
+        y0: 0,
+        hovered: null
+      };
+    },
+    created() {
+      this.xScale = scaleTime().range([margin.left, width - margin.right]);
+      this.yScale = scaleLinear().range([height - margin.bottom, margin.top]);
+      this.colorScale = scaleSequential(interpolateViridis);
+
+      this.areaGen = area().curve(curveCatmullRom);
+
+      this.xAxis = axisBottom()
+        .scale(this.xScale)
+        .tickFormat(this.format);
+      this.yAxis = axisLeft()
+        .scale(this.yScale)
+        .tickSizeOuter(0)
+        .tickFormat(d => `${format("$")(parseInt(d / 1000000))}M`);
+    },
+    watch: {
+      movies: function() {
+        this.calculateScales();
+        select(this.$refs.xAxis).call(this.xAxis);
+        select(this.$refs.yAxis)
+          .call(this.yAxis)
+          .select(".domain")
+          .remove();
+      },
+      filtered: function() {
+        this.calculateData();
+      }
+    },
+    methods: {
+      calculateScales: function() {
+        if (!this.movies.length) return;
+
+        // calculate median box office
+        this.medianBox = median(this.movies, d => d.boxOffice);
+
+        // colors
+        const colorDomain = extent(this.movies, d => d.score);
+        this.colorScale.domain(colorDomain).nice();
+
+        // x scale with dates
+        const [minDate, maxDate] = extent(this.movies, d => d.date);
+        this.xScale.domain([
+          timeMonth.offset(minDate, -2),
+          timeMonth.offset(maxDate, 2)
+        ]);
+
+        // y scale with boxOffice - medianBox
+        const yExtent = extent(this.movies, d => d.boxOffice - this.medianBox);
+        this.yScale.domain(yExtent);
+        this.y0 = this.yScale(0);
+
+        // update area generater
+        this.areaGen.y0(this.y0);
+      },
+      calculateData: function() {
+        if (!this.filtered.length) return;
+
+        // calculate arcs from the filtered movies
+        this.arcs = chain(this.filtered)
+          // put biggest arcs in the background
+          .sortBy(d => -Math.abs(d.boxOffice - this.medianBox))
+          .map(d => {
+            const x = this.xScale(d.date);
+            const y = this.yScale(d.boxOffice - this.medianBox);
+            // for each arc, just need d & fill
+            return {
+              id: d.id,
+              path: this.areaGen([
+                [this.xScale(timeMonth.offset(d.date, -2)), this.y0],
+                [x, y],
+                [this.xScale(timeMonth.offset(d.date, 2)), this.y0]
+              ]),
+              fill: this.colorScale(d.score),
+              data: d,
+              x,
+              y
+            };
+          })
+          .value();
+      },
+      enter: function(el, done) {
+        TweenLite.fromTo(
+          el,
+          0.25,
+          { scaleY: 0, svgOrigin: `0 ${this.y0}` },
+          { scaleY: 1, onComplete: done }
+        );
+      },
+      leave: function(el, done) {
+        TweenLite.fromTo(
+          el,
+          0.25,
+          { scaleY: 1 },
+          { scaleY: 0, svgOrigin: `0 ${this.y0}`, onComplete: done }
+        );
+      },
+      formatDate: function(date) {
+        return timeFormat("%b %d, %Y")(date);
+      },
+      formatBox: function(box) {
+        return format("$,.0f")(box);
+      }
     }
-  },
-  created() {
-    this.xScale = scaleTime().range([margin.left, width - margin.right]);
-    this.yScale = scaleLinear().range([height - margin.bottom, margin.top]);
-    this.colorScale = scaleSequential(interpolateViridis);
-
-    this.areaGen = area().curve(curveCatmullRom);
-
-    this.xAxis = axisBottom().scale(this.xScale).tickFormat(this.format);
-    this.yAxis = axisLeft().scale(this.yScale)
-      .tickSizeOuter(0)
-      .tickFormat(d => `${format('$')(parseInt(d / 1000000))}M`);
-  },
-  watch: {
-    movies: function() {
-      this.calculateScales()
-      select(this.$refs.xAxis).call(this.xAxis)
-      select(this.$refs.yAxis).call(this.yAxis)
-        .select('.domain').remove();
-    },
-    filtered: function() {
-      this.calculateData();
-    }
-  },
-  methods: {
-    calculateScales: function() {
-      if (!this.movies.length) return;
-
-      // calculate median box office
-      this.medianBox = median(this.movies, d => d.boxOffice);
-
-      // colors
-      const colorDomain = extent(this.movies, d => d.score);
-      this.colorScale.domain(colorDomain).nice();
-
-      // x scale with dates
-      const [minDate, maxDate] = extent(this.movies, d => d.date);
-      this.xScale.domain([
-        timeMonth.offset(minDate, -2),
-        timeMonth.offset(maxDate, 2),
-      ]);
-
-      // y scale with boxOffice - medianBox
-      const yExtent = extent(this.movies, d => d.boxOffice - this.medianBox);
-      this.yScale.domain(yExtent);
-      this.y0 = this.yScale(0);
-
-      // update area generater
-      this.areaGen.y0(this.y0);
-    },
-    calculateData: function() {
-      if (!this.filtered.length) return;
-
-      // calculate arcs from the filtered movies
-      this.arcs = chain(this.filtered)
-        // put biggest arcs in the background
-        .sortBy(d => -Math.abs(d.boxOffice - this.medianBox))
-        .map(d => {
-          const x = this.xScale(d.date);
-          const y = this.yScale(d.boxOffice - this.medianBox);
-          // for each arc, just need d & fill
-          return {
-            id: d.id,
-            path: this.areaGen([
-              [this.xScale(timeMonth.offset(d.date, -2)), this.y0],
-              [x, y],
-              [this.xScale(timeMonth.offset(d.date, 2)), this.y0],
-            ]),
-            fill: this.colorScale(d.score),
-            data: d, x, y,
-          };
-        }).value();
-    },
-    enter: function (el, done) {
-      TweenLite.fromTo(el, 0.25,
-        {scaleY: 0, svgOrigin: `0 ${this.y0}`},
-        {scaleY: 1, onComplete: done}
-      );
-    },
-    leave: function (el, done) {
-      TweenLite.fromTo(el, 0.25,
-        {scaleY: 1},
-        {scaleY: 0, svgOrigin: `0 ${this.y0}`, onComplete: done}
-      );
-    },
-    formatDate: function(date) {
-      return timeFormat('%b %d, %Y')(date);
-    },
-    formatBox: function(box) {
-      return format("$,.0f")(box);
-    }
-  }
-}
+  };
 </script>
 
 <style>
-.areachart {
-  position: relative;
-}
+  .areachart {
+    position: relative;
+  }
 
-.arc {
-  cursor: pointer;
-}
+  .arc {
+    cursor: pointer;
+  }
 
-.hovered {
-  position: absolute;
-  padding: 5px 10px;
-  background-color: #fff;
-  box-shadow: 0 0 5px #cfcfcf;
-  transform: translate(-50%);
-  pointer-events: none;
-  line-height: 1.6;
-  width: 240px;
-}
+  .hovered {
+    position: absolute;
+    padding: 5px 10px;
+    background-color: #fff;
+    box-shadow: 0 0 5px #cfcfcf;
+    transform: translate(-50%);
+    pointer-events: none;
+    line-height: 1.6;
+    width: 240px;
+  }
 </style>
